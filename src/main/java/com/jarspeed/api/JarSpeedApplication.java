d1e3bb72099283dc0c7f1d04c7b277085e6fd3ed
Courbaize <103497891+Courbaize@users.noreply.github.com>
2023-11-30 13:21:13 +0100

    Initial commit

---
+++ src/main/java/com/jarspeed/api/JarSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.jarspeed.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarSpeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarSpeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarspeed/api/JarSpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarspeed.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarSpeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
